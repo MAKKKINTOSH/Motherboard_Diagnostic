50b0bd3ed0b49b0e0ecb8855f136ce6ab3fd750c
eleleech <116322945+eleleech@users.noreply.github.com>
2024-05-08 12:26:01 +0800

    Вставила 2 неисправности на RTC и на PCIE

--- motherboard/Motherboard.cs
+++ motherboard/Motherboard.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Motherboard_Diagnostic.motherboard;
+using Motherboard_Diagnostic.motherboard.components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,10 +11,14 @@ namespace Motherboard_Diagnostic
     class Motherboard
     {
         public static Power Power;
+        public static Biosbattery Biosbattery;
+        public static PCInterface PCInterface;
 
         public static void Init()
         {
             Power = new();
+            Biosbattery = new();
+            PCInterface = new();
         }
     }
 }
